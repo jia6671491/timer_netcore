7d5a482ccf106291ba21338842f882739dd7cc5e
Jiaq <element2007jia@outlook.com>
2022-09-29 17:42:25 -0700

    Add project files.

---
+++ timer/Controllers/DataController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using timer.Models;
+
+namespace timer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        public DataController()
+        {
+
+        }
+
+        [Route("data")]
+        [HttpGet]
+        public async Task<ActionResult> getData()
+        {
+            XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(Inventory));
+
+            using StreamReader sr = new StreamReader("xmlData.xml");
+
+            return Ok(new {
+                errCode = 0,
+                errMsg = "",
+                data = (Inventory)ser.Deserialize(sr)
+            });
+        }
+
+        [Route("notify")]
+        [HttpGet]
+        public async Task<ActionResult> notify()
+        {
+            return Ok(new
+            {
+                errCode = 0,
+                errMsg = "Acknowledgement about End of Counting from server!",
+                data = new {}
+            });
+        }
+    }
+}
---
+++ timer/Models/Inventory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace timer.Models
+{
+    [XmlRoot(ElementName = "inventory")]
+    public class Inventory
+    {
+        public Inventory()
+        {
+            items = new List<item>();
+        }
+
+        [XmlElement(ElementName = "item")]
+        public List<item> items { get; set; }
+
+        public item this[string des]
+        {
+            get { return items.FirstOrDefault(s => string.Equals(s.Description, des, StringComparison.OrdinalIgnoreCase)); }
+        }
+    }
+
+    public class item
+    {
+        [XmlElement("Description")]
+        public string Description { get; set; }
+
+        [XmlElement("Count")]
+        public int Count { get; set; }
+    }
+}
